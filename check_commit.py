5667b25c01e290237f214f702e7373b2c9660035
Gabriel Santos Souza <gssouza2051@gmail.com>
2024-10-08 01:35:58 -0300

    'testing'

---
+++ check_commit.py
@@ -0,0 +1,17 @@
+import git
+
+def check_commit_message(repo_path, keywords):
+    repo = git.Repo(repo_path)
+    commits = list(repo.iter_commits('HEAD'))
+
+    for commit in commits:
+        for keyword in keywords:
+            if keyword in commit.message:
+                print(f"Commit {commit.hexsha} contém a palavra-chave: {keyword}")
+            else:
+                print(f"Commit {commit.hexsha} não contém nenhuma das palavras-chave.")
+
+# Exemplo de uso:
+repo_path = 'C:\Users\biel_\OneDrive\Documentos\FACULDADE\2024.2\Técnias_de_Eng_de_Software\AvaliacaoA3\A3\AvaliacaoA3'  # Substitua pelo caminho do seu repositório
+keywords = ['feat', 'fix', 'docs','refactor', 'test', 'chore', 'style']
+check_commit_message(repo_path, keywords)
