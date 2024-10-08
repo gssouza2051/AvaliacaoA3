6195db0e01d0eae80999122618e5c5b2b3bdc2bd
Gabriel Santos Souza <gssouza2051@gmail.com>
2024-10-08 09:32:06 -0300

    test: testando o conventional commits

--- check-commit.py
+++ check-commit.py
@@ -1,25 +1,5 @@
 import git
 import os
-import sys
-
-def check_commit_message(repo_path, keywords):
-    repo = git.Repo(repo_path)
-    last_commit = repo.head.commit
-    palavra_chave = False
-
-    for keyword in keywords:
-        if keyword in last_commit.message:
-            print(f"Commit {last_commit.hexsha} contém a palavra-chave: {keyword}")
-            palavra_chave = True  # Encontramos a palavra-chave, podemos sair da função
-
-    if palavra_chave == False:
-        print(f"O último commit {last_commit.hexsha} não contém nenhuma das palavras-chave!")
-
-    
-
-# Exemplo de uso:
-repo_path = os.getcwd()
-keywords = ['feat', 'fix', 'docs','refactor', 'test', 'chore', 'style']
 
 '''
 Conventional Commits
@@ -32,4 +12,26 @@ Conventional Commits
 6.test: -> Adição ou modificação de testes
 7.chore: -> Tarefas de manutenção,ajustes na configuração,etc.'''
 
-check_commit_message(repo_path, keywords)
+def main():
+    # Exemplo de uso:
+    repo_path = os.getcwd()
+    keywords = ['feat', 'fix', 'docs','refactor', 'test', 'chore', 'style']
+
+    check_commit_message(repo_path, keywords)
+
+def check_commit_message(repo_path, keywords):
+    repo = git.Repo(repo_path)
+    last_commit = repo.head.commit
+    palavra_chave = False
+
+    print(f'Mensagem do ultimo commit: {last_commit.message}')
+    for keyword in keywords:
+        if keyword in last_commit.message:
+            print(f"Commit {last_commit.hexsha} contém a palavra-chave: {keyword}")
+            palavra_chave = True  # Encontramos a palavra-chave, podemos sair da função
+
+    if palavra_chave == False:
+        print(f"O último commit {last_commit.hexsha} não contém nenhuma das palavras-chave!")
+
+    
+main()
