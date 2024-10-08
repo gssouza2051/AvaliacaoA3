a16d4f50c7392c473998eba8c60c70c51b2f28d5
Gabriel Santos Souza <gssouza2051@gmail.com>
2024-10-08 02:09:48 -0300

    'testing'

--- check-commit.py
+++ check-commit.py
@@ -12,6 +12,6 @@ def check_commit_message(repo_path, keywords):
                 print(f"Commit {commit.hexsha} não contém nenhuma das palavras-chave!")
 
 # Exemplo de uso:
-repo_path = 'C:\Users\biel_\OneDrive\Documentos\FACULDADE\2024.2\Técnias_de_Eng_de_Software\AvaliacaoA3\A3\AvaliacaoA3'  # Substitua pelo caminho do seu repositório
+repo_path = r'C:\Users\biel_\OneDrive\Documentos\FACULDADE\2024.2\Técnias_de_Eng_de_Software\AvaliacaoA3\A3\AvaliacaoA3'
 keywords = ['feat', 'fix', 'docs','refactor', 'test', 'chore', 'style']
 check_commit_message(repo_path, keywords)
