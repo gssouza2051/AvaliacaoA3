448072a833b5ae01ba361543b802279def995d4e
Gabriel Santos Souza <gssouza2051@gmail.com>
2024-10-08 02:18:34 -0300

    tt

--- check-commit.py
+++ check-commit.py
@@ -1,16 +1,23 @@
 import git
 import os
+import sys
 
 def check_commit_message(repo_path, keywords):
     repo = git.Repo(repo_path)
     commits = list(repo.iter_commits('HEAD'))
+    palavra_chave = False
 
     for commit in commits:
         for keyword in keywords:
             if keyword in commit.message:
                 print(f"Commit {commit.hexsha} contém a palavra-chave: {keyword}")
+                palavra_chave = True
+                break
             else:
                 print(f"Commit {commit.hexsha} não contém nenhuma das palavras-chave!")
+    if palavra_chave == False:
+        sys.exit()
+    
 
 # Exemplo de uso:
 repo_path = os.getcwd()
