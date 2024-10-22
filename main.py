e87263202438df6c3c75750764dfcae96ef48e6f
Gabriel Santos Souza <gssouza2051@gmail.com>
2024-10-22 16:15:30 -0300

    fix: corrigindo bug da main, na lib do customtkinter

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from modules import janela as page
 
 
 def main():
-    janela = page.config()
-    page.janela_login()
+    #janela = page.config()
+    #page.janela_login()
+    print('.')
 
 if __name__ == "__main__":
     main()
